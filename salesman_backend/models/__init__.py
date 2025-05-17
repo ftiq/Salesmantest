06fa2fb69fabe0b99d8ad1b71ccb15bdfa4acf2a
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-17 16:12:04 +0300

    Delete salesman_backend directory

--- salesman_backend/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import salesman_profile
-from . import route
-from . import daily_route
-from . import daily_route_line  # أضف هذا السطر
-from . import visit
-from . import res_partner
-from . import account_payment
