3432bfafe248bf02b2019348462ae2efe23bd382
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-17 14:35:37 +0300

    Update __manifest__.py

--- salesman_backend/__manifest__.py
+++ salesman_backend/__manifest__.py
@@ -1,12 +1,30 @@
 {
-    'name': 'Salesman Backend',
-    'version': '1.0',
-    'summary': 'إدارة المناديب والمسارات',
-    'description': 'نظام متكامل لإدارة فرق المبيعات الميدانية',
+    'name': 'إدارة المناديب الميدانيين',
+    'version': '1.0.0',
+    'summary': 'نظام متابعة المناديب وخطط الزيارات الأسبوعية',
+    'description': """
+        نظام لإدارة:
+        - ملفات المناديب
+        - خطط الزيارات الأسبوعية
+        - المسارات المحددة مسبقاً
+        - سجل الزيارات
+    """,
     'author': 'Your Company',
     'website': 'https://www.yourcompany.com',
     'category': 'Sales',
-    'depends': ['base', 'sale', 'account', 'contacts'],
+    'depends': ['base', 'contacts', 'account'],
+    'data': [
+        # ملفات الأمان
+        'security/ir_rule.xml',
+        'security/ir.model.access.csv',
+        
+        # ملفات العروض الأساسية
+        'views/menu.xml',
+        'views/salesman_views.xml',
+        'views/weekly_route_views.xml',
+        'views/route_views.xml',
+        'views/visit_views.xml',
+    ],
     'installable': True,
     'application': True,
     'auto_install': False,
