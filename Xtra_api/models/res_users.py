235e0c86021e5ecf7bc4fdc9f542c165f20cc3bb
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-22 19:03:05 +0300

    Update res_users.py

--- Xtra_api/models/res_users.py
+++ Xtra_api/models/res_users.py
@@ -1,6 +1,13 @@
-from odoo import models, fields
+from odoo import models, fields, api
+import secrets
 
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    api_token = fields.Char(string="s0KJ%d012A]-", readonly=True)
+    api_token = fields.Char(string="API Token", readonly=True)
+
+    @api.model
+    def create(self, vals):
+        if not vals.get('api_token'):
+            vals['api_token'] = secrets.token_urlsafe(32)
+        return super().create(vals)
