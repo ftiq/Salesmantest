2c3b148b7d958dab01acd93aa0e20a1e38017deb
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-22 19:42:14 +0300

    Update res_users.py

--- Xtra_api/models/res_users.py
+++ Xtra_api/models/res_users.py
@@ -3,4 +3,4 @@ from odoo import models, fields
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    api_token = fields.Char(string="API Token", copy=False, index=True)
+    api_token = fields.Char("API Token", readonly=False)
