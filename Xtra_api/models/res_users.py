aa106b4700dcea60f18d699cff6e84b5af75ccbb
MHD Manar Zamrik <manar.z@ft.iq>
2025-06-01 02:10:39 +0300

    Update res_users.py

--- Xtra_api/models/res_users.py
+++ Xtra_api/models/res_users.py
@@ -1,6 +1,12 @@
-from odoo import models, fields
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
 
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    api_token = fields.Char("API Token", readonly=False)
+    api_token = fields.Char("API Token", readonly=True)
+
+    def write(self, vals):
+        if 'api_token' in vals and vals['api_token'] != self.api_token:
+            raise UserError(_("You cannot change the API Token once it is set."))
+        return super().write(vals)
